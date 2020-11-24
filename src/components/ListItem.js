b8c40f3fba3c385201138c8a2f20cb61dfba30b5
karthiik433 <kartheek12345kumar@gmail.com>
2020-11-24 20:18:53 +0530

    update

---
+++ src/components/ListItem.js
@@ -0,0 +1,33 @@
+import React,{useState} from "react";
+import "./../styles/App.css";
+
+function ListItem(props) 
+{
+    const [editedItem,setEditedItem] = useState(props.item);
+    const [editMode,setEditMode] = useState(false);
+    const editedItemChanged = (evnt) =>{
+		setEditedItem(evnt.target.value);
+    }
+    const saveEditedItem = () =>{
+        props.editHandler(editedItem,props.idx);
+        setEditMode(false);
+
+    }
+	return (
+	<div className="list">
+        {editMode ?
+        (<>
+        <textarea className="editTask" onChange={editedItemChanged} placeholder="Edit Task" value={editedItem}></textarea>
+        <button className="saveTask" onClick={saveEditedItem} disabled={editedItem.trim().length ===0}>Save Task</button>
+        </>) : <>
+         {props.item}
+         <button className="edit" onClick={ ()=> setEditMode(true)}>edit</button>
+         <button className="delete" onClick={() => props.deleteHandler(props.idx)}>delete</button>
+        </>
+        }
+    </div>
+	);
+}
+
+
+export default ListItem;
