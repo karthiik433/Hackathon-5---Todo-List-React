124798656f33ecd21be9bec1545b1e01e5ea03ba
karthiik433 <kartheek12345kumar@gmail.com>
2021-01-04 20:12:27 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import ListItem from "./ListItem";
 import "./../styles/App.css";
 
@@ -6,30 +6,109 @@ function App()
 {
 	const [items,setItems] = useState([]);
 	const [newItem,setNewItem] = useState("");
+	
+	const setDoneHandler = (idx)=>{
+		const todoId = items[idx]._id;
+		var requestOptions = {
+		  method: 'PUT',
+		  redirect: 'follow'
+		};
+		
+		fetch(`http://localhost:9990/updateDone/${todoId}`, requestOptions)
+		  .then(response => response.json())
+		  .then(result => console.log(result))
+		  .catch(error => console.log('error', error));
+		  
+
+	} 
 	const addItem = () =>{
-		items.push(newItem);
-		//setItems(items);
-		setItems([...items]);
-       setNewItem("");
+		
+		var myHeaders = new Headers();
+ 		myHeaders.append("Content-Type", "application/json");
+
+			var raw = JSON.stringify({"task":newItem,"done":false});
+
+			var requestOptions = {
+  			method: 'POST',
+  			headers: myHeaders,
+ 			body: raw,
+  			redirect: 'follow'
+};
+
+				fetch("http://localhost:9990/createTodo", requestOptions)
+  				.then(response => response.json())
+  				.then(result => {
+					  console.log(result)
+					  items.push(result);
+					  setItems([...items]);
+        			  setNewItem("");
+				  })
+  				.catch(error => console.log('error', error));
+				//setItems(items);
+				
 	}
 	const newItemChanged = (evnt) =>{
 		setNewItem(evnt.target.value);
 	}
 	const deleteHandler = (itemIdx) =>{
+		const todoId = items[itemIdx]._id;
+		var requestOptions = {
+		  method: 'DELETE',
+		  redirect: 'follow'
+		};
+		
+		fetch(`http://localhost:9990/todo/${todoId}`, requestOptions)
+		  .then(response => response.text())
+		  .then(result => console.log(result))
+		  .catch(error => console.log('error', error));
 		items.splice(itemIdx,1);
 		setItems([...items]);
 	}
 	const editHandler = (editedValue,itemIdx) =>{
-		items[itemIdx] = editedValue;
+         const todoId = items[itemIdx]._id;
+
+		 var myHeaders = new Headers();
+		 myHeaders.append("Content-Type", "application/json");
+		 
+		 var raw = JSON.stringify({"task":editedValue});
+		 
+		 var requestOptions = {
+		   method: 'PUT',
+		   headers: myHeaders,
+		   body: raw,
+		   redirect: 'follow'
+		 };
+		 
+		 fetch(`http://localhost:9990/todo/${todoId}`, requestOptions)
+		   .then(response => response.text())
+		   .then(result => console.log(result))
+		   .catch(error => console.log('error', error));
+		items[itemIdx].task = editedValue;
 		setItems([...items]);
 	}
+	useEffect(()=>{
+			var requestOptions = {
+  			method: 'GET',
+  			redirect: 'follow'
+		};
+
+		fetch("http://localhost:9990/todo", requestOptions)
+  		.then(response => response.json())
+  		.then(result => setItems(result))
+  		.catch(error => console.log('error', error));
+	},[])
 	return (
 	<div id="main">
-	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}></textarea>
-<button id="btn" onClick={addItem} disabled={newItem.trim().length === 0}>Add Item</button>
+	<div>
+	<h1>Todo List By Karthik</h1>
+    </div>
+	<div className="toplevel">		
+	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem} style={{width:"500px"}}></textarea>
+    <button id="btn" className="btn-warning" onClick={addItem} disabled={newItem.trim().length === 0}>Add Item</button>
+    </div>
 	{
       items.map((item,idx) =>(
-		  <ListItem item={item} key={`${item}_${idx}`} idx={idx} editHandler={editHandler} deleteHandler={deleteHandler} />
+		  <ListItem item={item}  key={`${item}_${idx}`} idx={idx} editHandler={editHandler} deleteHandler={deleteHandler} setDoneHandler={setDoneHandler}  />
 	  ))
 	}
 	</div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,8 +3,10 @@ import "./../styles/App.css";
 
 function ListItem(props) 
 {
-    const [editedItem,setEditedItem] = useState(props.item);
+    const [editedItem,setEditedItem] = useState(props.item.task);
     const [editMode,setEditMode] = useState(false);
+    const [doneMode,setDoneMode] = useState(false);
+    
     const editedItemChanged = (evnt) =>{
 		setEditedItem(evnt.target.value);
     }
@@ -13,18 +15,39 @@ function ListItem(props)
         setEditMode(false);
 
     }
+    
 	return (
 	<div className="list">
+        {props.item.done || doneMode ?
+        <div className="todos">
+        {props.item.task}
+        <span className="float-buttons">
+        <button className="delete btn-danger" onClick={() => props.deleteHandler(props.idx)}>delete</button>
+        <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2 tick" viewBox="0 0 16 16">
+  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+</svg></span>
+        </span>
+        </div>
+        :
+        <>
         {editMode ?
-        (<>
-        <textarea className="editTask" onChange={editedItemChanged} placeholder="Edit Task" value={editedItem}></textarea>
-        <button className="saveTask" onClick={saveEditedItem} disabled={editedItem.trim().length ===0}>Save Task</button>
-        </>) : <>
-         {props.item}
-         <button className="edit" onClick={ ()=> setEditMode(true)}>edit</button>
-         <button className="delete" onClick={() => props.deleteHandler(props.idx)}>delete</button>
-        </>
+        (<div className="todos-save">
+        <textarea className="editTask" style={{width:"400px"}} onChange={editedItemChanged} placeholder="Edit Task" value={editedItem}></textarea>
+        <button className="saveTask btn-success" onClick={saveEditedItem} disabled={editedItem.trim().length ===0}>Save Task</button>
+        </div>) : <div className="todos">
+         {props.item.task}
+         <span className="float-buttons">
+         <button className="edit btn-success" onClick={ ()=> setEditMode(true)}>edit</button>
+         <button className="delete btn-danger" onClick={() => props.deleteHandler(props.idx)}>delete</button>
+         <button className="done btn-primary" onClick={()=>{
+             props.setDoneHandler(props.idx);
+             setDoneMode(true);
+             }}>Done</button>
+         </span>
+        </div>
         }
+        </>
+    }
     </div>
 	);
 }
